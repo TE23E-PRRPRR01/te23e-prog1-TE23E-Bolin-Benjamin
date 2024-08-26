0f83def2957038bb9aa4eb341ce56e5e106a4860
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2024-08-26 14:31:51 +0200

    Tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen");
+Console.WriteLine("Nu mitt första C#-program");
+
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+
+Console.WriteLine("vad gör du på fritiden?");
+string fritiden = Console.ReadLine();
+Console.WriteLine(fritiden  + "jag gillar också att göra det på min fritid");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkBlue
+
