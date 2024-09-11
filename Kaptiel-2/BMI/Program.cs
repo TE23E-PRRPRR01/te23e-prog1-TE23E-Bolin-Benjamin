38fa3a12a2692d3aa31b907bab755ab144b47bdc
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2024-09-11 19:25:38 +0200

    omvandlat fareheit, celcius samt att bmi samt hur mycket det kostar att hyra bil

---
+++ Kaptiel-2/BMI/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program som informerar användaren om deras BMI (Body Mass Index)
+Console.Clear();
+Console.WriteLine("Program som informerar användaren om deras BMI (Body Mass Index)");
+
+// Läsa in vikten i kilogram
+Console.Write("Ange din vikt i kilogram: ");
+double viktKilogram = double.Parse(Console.ReadLine());
+
+// Läsa in längd i meter
+Console.Write("Ange din längd i meter: ");
+double längdMeter = double.Parse(Console.ReadLine());
+
+// Räkna ut användarens BMI
+double BMI = viktKilogram / (längdMeter * längdMeter);
+
+// Berätta till användaren om vad deras BMI är
+Console.WriteLine($"Din BMI är {BMI:00.0}");
---
+++ Kaptiel-2/Biluthyrning/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program för att räkna ut kostnaden för att hyra en bil
+Console.Clear();
+Console.WriteLine("Program för att räkna ut kostnaden för att hyra en bil");
+
+// Läsa in hur mycket mil bilen har kört
+Console.Write("Ange hur mycket mil bilen har kört: ");
+int mil = int.Parse(Console.ReadLine());
+
+// Läsa in hur många dagar användaren ska hyra bilen
+Console.Write("Ange hur många dagar du vill hyra bilen: ");
+int dagar = int.Parse(Console.ReadLine());
+
+// Räkna ut hur mycket det kommer att kosta för att hyra bilen
+double pris = mil * 5 + dagar * 100 + 500;
+
+// Berätta hur mycket kronor det kommer att kosta
+Console.WriteLine($"Då kommer bilen att kosta dig {pris} kronor");
