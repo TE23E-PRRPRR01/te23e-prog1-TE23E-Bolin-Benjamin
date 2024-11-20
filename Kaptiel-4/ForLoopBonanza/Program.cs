393100f0d10f1e476deb9dabe0a1d342982afd7b
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2024-11-20 14:53:33 +0100

    tränat på for loopar, jämna/udda siffror

---
+++ Kaptiel-4/ForLoopBonanza/Program.cs
@@ -0,0 +1,35 @@
+﻿// for-loopar för att iterera över siffror, göra beräkningar och skapa mönster.
+Console.Clear();
+
+// Console.WriteLine("Ett program för att göra for-loopar för att iterera över siffror, göra beräkningar och skapa mönster.");
+
+// Console.WriteLine("Ange startvärde");
+// string mintext = Console.ReadLine();
+// int min = 0;
+// bool success = int.TryParse(mintext, out min);
+
+// Console.WriteLine("Ange slutvärde");
+// string maxText = Console.ReadLine();
+// int max = 0;
+// success = int.TryParse(maxText, out max);
+
+// int summa = 0;
+
+// for (int i = min; i < max + 1; i++)
+// {
+//     Console.WriteLine(i + 1);
+//     summa = i;
+// }
+
+// Console.WriteLine($"Summan av siffrorna {min} till {max}är {summa}");
+
+Console.WriteLine("Jämna siffror: ");
+
+for (int i = 0; i < 11; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
+
---
+++ Kaptiel-4/ForLoopTryPars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+Console.Clear();
+Console.WriteLine("Skapa en loop som körs 10 gånger");
+
+
+Console.Write("Skriv ditt namn: ");
+string namn = Console.ReadLine();
+
+while (true)
+{
+    Console.Write("Antal loopar: ");
+    int antal = int.TryParse(Console.ReadLine());
+
+    for (int i = 0; i < antal; i++)
+    {
+        Console.WriteLine($" {i}. {namn}");
+    }
+
+}
