e783e8bebc5a35c7d6cfd856d5c997173bb78799
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2024-11-25 14:50:15 +0100

    Klar mned listor

---
+++ Kaptiel-5/Microovningar1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+//Skapa en lista med filmnamn
+List<string> listaNamn = ["Anna", " Björn", " Cecilia"];
+
+//Skriv ut alla filmnamn
+Console.WriteLine($"Nuvarande lista: {string.Join(",", listaNamn)}");
+
+Console.WriteLine("Ange ett namn att lägga till:");
+string namn = Console.ReadLine();
+
+listaNamn.Add(namn);
+
+Console.WriteLine($"Nuvarande lista: {string.Join(",", listaNamn)}");
+
+Console.WriteLine("Ange ett namn att ta bort:");
+string bort = Console.ReadLine();
+
+//Om den finns ta bort den
+if (listaNamn.Contains(bort))
+{
+    Console.WriteLine("Namnet finns");
+    listaNamn.Remove("bort");
+}
+else
+{
+    Console.WriteLine("Namnet finns inte");
+
+}
+
+Console.WriteLine($"Nuvarande lista: {string.Join(",", listaNamn)}");
+
