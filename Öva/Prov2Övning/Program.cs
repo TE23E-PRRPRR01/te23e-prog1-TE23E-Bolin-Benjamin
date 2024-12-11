984a0a47d534a9332b8bd54a4c82049eaf097927
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2024-12-11 15:03:34 +0100

    övningsprov

---
+++ Öva/Prov2Övning/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+List<string> names = ["Anna", "Björn", "Cecilia", "Daniel", "Eva"];
+Console.WriteLine($"Mina kompisar är: {string.Join(", ", names)}");
+Console.WriteLine("Första namnet är " + names.First());
+Console.WriteLine("Sista namnet är " + names.Last());
+
+int size = names.Count();
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"{i + 1}. {names[i]}");
+}
+
+//List<int> numbers = [1000, 1001, 1002, 1005, 1006];
+//Console.WriteLine($"Fem viktiga årtal är: {string.Join(", ", numbers)}");
+
+
+List<string> drickor = ["Cola", "Te", "Pepsi", "Fanta", "Vatten"];
+
+static void printadrickor(List<string> drickor)
+{
+    Console.WriteLine("Mina favoritdrycker är:");
+    int length = drickor.Count();
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {drickor[i]}");
+    }
+
+}
+
+printadrickor(drickor);
+
+Console.WriteLine("Lägg till din favorit dryck:");
+string nydricka = Console.ReadLine();
+drickor.Add(nydricka);
+printadrickor(drickor);
+
+List<int> years = [1492, 1776];
+
+static void printyears(List<int> myyears)
+{
+    int length = myyears.Count();
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {myyears[i]}");
+    }
+}
+
+printyears(years);
+
+Console.WriteLine("Lägg till ett år:");
+int y = int.TryParse(Console.ReadLine());
+
+years.Add(y);
+
+printyears(years);
+
---
+++ Öva/prov2/Program.cs
@@ -0,0 +1,88 @@
+﻿Console.Clear();
+Console.WriteLine("Filmregister");
+
+//Lista för att lagra filmer
+List<string> listafilmer = [];
+
+//Be användaren ange ett antal
+Console.Write("Ange antal filmer per sida (3-5): ");
+string antalText = Console.ReadLine();
+int antal = 0;
+while (true)
+{
+    bool success = int.TryParse(antalText, out antal);
+    if (success)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Fel! Var gof att mata in ett heltal.");
+    }
+}
+
+//Programloop
+while (true)
+{
+    Console.WriteLine();
+    //visa en meny
+    Console.Write("""
+1. Lägg till filmer
+2. Lista alla filmer
+3. Sök en film
+4. Avsluta
+Välj ett alternativ:
+""");
+    string val = Console.ReadLine();
+
+    //Hanterar med valet
+    if (val == "1")
+    {
+        Console.WriteLine();
+        //Lägg till filmer i listan
+        for (int i = 0; i < antal; i++)
+        {
+            Console.Write("Ange en film att lägga till: ");
+            string film = Console.ReadLine();
+            listafilmer.Add(film);
+        }
+    }
+    else if (val == "2")
+    {
+        Console.WriteLine();
+        //Skriv ut alla filmer
+        foreach (var filmnamn in listafilmer)
+        {
+            Console.WriteLine($"- {filmnamn}");
+        }
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine();
+        //Sök en film
+        Console.WriteLine("Ange en film att söka efter (första bokstaven): ");
+        string bokstav = Console.ReadLine();
+        foreach (var filmnamn in listafilmer)
+        {
+            // Kolla om varje film börjar på bokstaven
+            if (filmnamn.StartsWith(bokstav))
+            {
+                Console.WriteLine(filmnamn);
+
+            }
+        }
+    }
+    else if (val == "4")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tack för idag!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Fel val. Försök igen!");
+    }
+
+}
+
